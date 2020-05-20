4b64d693675ab63571e07acd654ded8292f06b7e
Pablo Novas <pjnovas@gmail.com>
2020-05-20 19:00:10 -0300

    updated verification

--- index.js
+++ index.js
@@ -3,41 +3,41 @@ const exec = require('child_process').exec;
 
 const express = require('express');
 const healthcheck = require('healthcheck-middleware');
-const nodeCleanup = require('node-cleanup');
-const { Webhooks } = require('@octokit/webhooks');
+const { verify } = require('@octokit/webhooks');
 
 const repos = require('./repos');
 const port = process.env.PORT || 8080;
 
 const app = express();
 
+app.use(express.json());
 app.use('/healthcheck', healthcheck());
 
-const hooks = Object.keys(repos).map(key => {
+Object.keys(repos).forEach(key => {
   const { secret, when, dir, run } = repos[key];
 
-  const handler = ({ name, payload }) => {
-    const cmd = run(payload, name);
+  app.post(`/${key}`, (req, res) => {
+    if (verify(secret, req.body, req.get('X-Hub-Signature'))) {
+      const name = req.get('X-GitHub-Event');
 
-    if (cmd) {
-      exec(`cd ${dir}`);
-      exec(cmd);
+      if (when === name) {
+        const cmd = run(payload, name);
+
+        if (cmd) {
+          exec(`cd ${dir}`);
+          exec(cmd);
+        }
+
+        res.sendStatus(200);
+        return;
+      }
+
+      res.status(400).send('event name unexpected');
+      return;
     }
-  };
 
-  const webhooks = new Webhooks({ secret });
-
-  app.post(`/${key}`, webhooks.middleware);
-  webhooks.on(when, handler);
-
-  return {
-    webhooks,
-    clean: () => webhooks.removeListener(when, handler)
-  };
+    res.sendStatus(401);
+  });
 });
 
 app.listen(port, () => console.log(`listening at ${port} port`));
-
-nodeCleanup(() => {
-  hooks.forEach(({ clean }) => clean());
-});
